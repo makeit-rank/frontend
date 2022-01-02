cea778fb7481e44e42ee3ad01a1aa76ddaf8afda
Raj <varasaniraj5@gmail.com>
2022-01-02 20:31:59 +0530

    Created navbar comp

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,33 +1,141 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import styles from "./NavBar.module.css";
 
 import { NAVBAR_DATA } from "../../Utils/Constants/StaticData";
+import { getCityFromPincode } from "./../../Services/location.service";
 
-function NavBar({ isLoggedIn, pincode, onPinClick }) {
-  const [city, setCity] = useState("");
+function NavBar({ isLoggedIn }) {
+  const location = useLocation();
+  const userData = useSelector((state) => state.userReducer.userData);
+
+  const searchInputRef = useRef(null);
+
+  const [locationInfo, setLocationInfo] = useState({
+    city: "",
+    pincode: "",
+  });
+
+  useEffect(() => {
+    console.log(location.pathname);
+  }, [location]);
+
+  useEffect(async () => {
+    let pincode = userData.addresses?.[0]?.pincode || "394107";
+    try {
+      const cityInfo = await getCityFromPincode(pincode);
+      if (cityInfo) {
+        setLocationInfo({
+          city: cityInfo,
+          pincode,
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }, [userData]);
 
   return (
     <div className={styles.Wrapper}>
       <div className={styles.LeftSec}>
-        <Link href="/">
-          <img src={NAVBAR_DATA.images.LOGO_IMG} alt="" />
+        <Link to="/" className={styles.LogoLink}>
+          <img
+            src={NAVBAR_DATA.images.LOGO_IMG}
+            alt=""
+            className={styles.Logo}
+          />
         </Link>
         <div className={styles.LocationInfo}>
-          <div className={styles.LocationInfoLeft}>
-            <div className={styles.LocationPin}>
-              <img src={NAVBAR_DATA.images.LOCATION_PIN} alt="" />
-            </div>
+          <div className={styles.LocationPin}>
+            <img
+              src={NAVBAR_DATA.images.LOCATION_PIN}
+              alt=""
+              className={styles.LocationPinImg}
+            />
           </div>
           <div className={styles.LocationInfoRight}>
             <h4 className={styles.DeliverToTitle}>{NAVBAR_DATA.deleiverTo}</h4>
             <div className={styles.LocationInfoRightBottom}>
-              <h3 className={styles.DeliverToArea}>{city}</h3>
-              <h3 className={styles.DeliverToPincode}>{pincode}</h3>
+              <h3 className={styles.DeliverToPincode}>
+                {locationInfo.pincode}
+              </h3>
+              <h3 className={styles.DeliverToArea}>{locationInfo.city}</h3>
             </div>
           </div>
         </div>
+      </div>
+      <form
+        className={styles.SearchBar}
+        onClick={(e) => {
+          searchInputRef.current.focus();
+        }}
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(e.target.elements.search.value);
+        }}
+      >
+        <div className={styles.SearchIconWrapper}>
+          <img
+            src={NAVBAR_DATA.images.SEARCH_ICON_IMG}
+            alt=""
+            className={styles.SearchBarImg}
+          />
+        </div>
+        <input
+          ref={searchInputRef}
+          type="text"
+          placeholder="Search for products"
+          className={styles.SearchBarInput}
+          title="Search for products"
+          name="search"
+        />
+      </form>
+      <div className={styles.RightSec}>
+        {isLoggedIn ? (
+          location.pathname !== "/profile" && (
+            <Link
+              to="/profile"
+              className={styles.ProfileButton + " " + styles.LinkButton}
+            >
+              <img
+                src={NAVBAR_DATA.images.PROFILE_IMG}
+                alt=""
+                className={
+                  styles.ProfileButtonImg + " " + styles.LinkButtonIcon
+                }
+              />
+              <h4
+                className={
+                  styles.ProfileButtonText + " " + styles.LinkButtonText
+                }
+              >
+                {NAVBAR_DATA.profie}
+              </h4>
+            </Link>
+          )
+        ) : (
+          <Link to="/login" className={styles.LoginButton}>
+            <h4 className={styles.LoginButtonText}>{NAVBAR_DATA.login}</h4>
+          </Link>
+        )}
+        {location.pathname !== "/cart" && (
+          <Link
+            to="/cart"
+            className={styles.CartButton + " " + styles.LinkButton}
+          >
+            <img
+              src={NAVBAR_DATA.images.CART_IMG}
+              alt=""
+              className={styles.CartButtonImg + " " + styles.LinkButtonIcon}
+            />
+            <h4 className={styles.CartButtonText + " " + styles.LinkButtonText}>
+              {NAVBAR_DATA.cart}
+            </h4>
+          </Link>
+        )}
       </div>
     </div>
   );
