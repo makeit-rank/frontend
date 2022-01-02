17f821c87da29718d526c2b2d646740b424fd0ce
Raj <varasaniraj5@gmail.com>
2022-01-02 20:25:42 +0530

    Updated navabr staticdata

--- src/Utils/Constants/StaticData.js
+++ src/Utils/Constants/StaticData.js
@@ -2,8 +2,8 @@ import logo from "../../Assets/_General/Logo.svg";
 import locationPin from "../../Assets/_General/LocationPin.svg";
 import profileImg from "../../Assets/_General/Profile.svg";
 import cartImg from "../../Assets/_General/Cart.svg";
+import searchIconImg from "../../Assets/_General/Search.svg";
 
-// import bgLineImg from "../../Assets/_General/bgLine.svg";
 import bgLineImg from "../../Assets/_General/BgLine.svg";
 
 export const BG_LINE_IMG = bgLineImg;
@@ -12,10 +12,12 @@ export const NAVBAR_DATA = {
   deleiverTo: "Deliver to",
   profie: "Profile",
   cart: "Cart",
+  login: "Login/Signup",
   images: {
     LOGO_IMG: logo,
     LOCATION_PIN: locationPin,
     PROFILE_IMG: profileImg,
     CART_IMG: cartImg,
+    SEARCH_ICON_IMG: searchIconImg,
   },
 };
