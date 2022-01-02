dfca2d2a4bd9bfe734e77cbf95deae526b99ca02
Raj <varasaniraj5@gmail.com>
2022-01-02 20:25:26 +0530

    Added api constants

--- src/Utils/Constants/ApiConstants.js
+++ src/Utils/Constants/ApiConstants.js
@@ -1,3 +1,6 @@
-PINCODE_TO_CITY_URL = "https://api.postalpincode.in/pincode/";
+export const PINCODE_TO_CITY_URL = "https://api.postalpincode.in/pincode/";
 
-BASE_URL = "https://makeit-test.azurewebsites.net/api/v1/";
+export const BASE_URL = "https://makeit-test.azurewebsites.net/api/v1/";
+export const USER_URL = BASE_URL + "user/";
+
+export const GET_USER_DATA = USER_URL + "getuserdata";
