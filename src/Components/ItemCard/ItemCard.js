7a6d9bb9b3b1d2db34b9717514711a0158a8481d
Raj <varasaniraj5@gmail.com>
2022-01-03 14:34:59 +0530

    Created item card

---
+++ src/Components/ItemCard/ItemCard.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./ItemCard.module.css";
+
+import Ratings from "../Ratings";
+import { ReactComponent as HeartComp } from "../../Assets/_General/Heart.svg";
+
+import { HOME_DATA } from "./../../Utils/Constants/StaticData";
+
+export default function ItemCard({ itemData }) {
+  const [selected, setSelected] = useState(false);
+
+  useEffect(() => {
+    // check if item is wishlisted
+    setSelected(Math.random() > 0.5);
+  }, []);
+
+  return (
+    <div className={styles.Wrapper}>
+      <Link to={`/p/${itemData.id}`}>
+        <div className={styles.UpperSec}>
+          <img src={itemData.image} alt="item" className={styles.Image} />
+          <div
+            className={styles.AddToWishList}
+            onClick={() => setSelected(!selected)}
+          >
+            <HeartComp
+              className={
+                styles.WishListHeart +
+                " " +
+                (selected ? styles.WishlistedHeart : "")
+              }
+            />
+          </div>
+        </div>
+        <div className={styles.LowerSec}>
+          <div className={styles.Seller}>{itemData.seller}</div>
+          <h5 className={styles.Title}>{itemData.title}</h5>
+          <div className={styles.Price}>
+            {"₹"} {itemData.price}
+          </div>
+          <div className={styles.Rating}>
+            <div className={styles.RatingWrapper}>
+              <Ratings rating={itemData.rating} />
+            </div>
+            <div className={styles.RatingCount}>
+              {itemData.noOfRatings} {HOME_DATA.ratings}
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
