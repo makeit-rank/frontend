d4b1d0e2113a3ab570ee3536df9a8776b5ba6157
Raj <varasaniraj5@gmail.com>
2022-01-03 14:31:49 +0530

    Updated static data

--- src/Utils/Constants/StaticData.js
+++ src/Utils/Constants/StaticData.js
@@ -4,10 +4,11 @@ import profileImg from "../../Assets/_General/Profile.svg";
 import cartImg from "../../Assets/_General/Cart.svg";
 import searchIconImg from "../../Assets/_General/Search.svg";
 import homePageImage from "../../Assets/Home/MainImage.png";
-
 import bgLineImg from "../../Assets/_General/BgLine.svg";
+import StarImg from "../../Assets/_General/Star.svg";
 
 export const BG_LINE_IMG = bgLineImg;
+export const STAR_IMG = StarImg;
 
 export const NAVBAR_DATA = {
   deleiverTo: "Deliver to",
@@ -26,6 +27,10 @@ export const NAVBAR_DATA = {
 export const HOME_DATA = {
   title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam",
   button: "See top picks ->",
+  recommended: "Recommended for You",
+  topPicks: "Top Picks",
+  viewAll: "View all ->",
+  ratings: "Reviews",
   images: {
     MAIN_IMAGE: homePageImage,
   },
