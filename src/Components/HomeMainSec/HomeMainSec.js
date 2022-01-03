b4de508767408eb5f31a275d6d66aae9bcef3089
Raj <varasaniraj5@gmail.com>
2022-01-03 15:24:04 +0530

    Added footer in home main sec

--- src/Components/HomeMainSec/HomeMainSec.js
+++ src/Components/HomeMainSec/HomeMainSec.js
@@ -2,6 +2,7 @@ import HighlightsItemSec from "./HighlightsItemSec";
 import styles from "./HomeMainSec.module.css";
 import UpperSec from "./UpperSec";
 import { HOME_DATA } from "./../../Utils/Constants/StaticData";
+import Footer from "./../Footer/Footer";
 
 const tempData = Array(4)
   .fill({})
@@ -30,6 +31,7 @@ function HomeMainSec({ recommendedItems = tempData, topPickItems = tempData }) {
         redirectTo={"/top-pick"}
         title={HOME_DATA.topPicks}
       />
+      <Footer />
     </div>
   );
 }
