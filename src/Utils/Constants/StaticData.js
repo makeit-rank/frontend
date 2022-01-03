18ea6db286fae744c2ee354fbf344bb5bd56ddbe
Raj <varasaniraj5@gmail.com>
2022-01-03 10:57:50 +0530

    Added home page static data

--- src/Utils/Constants/StaticData.js
+++ src/Utils/Constants/StaticData.js
@@ -3,6 +3,7 @@ import locationPin from "../../Assets/_General/LocationPin.svg";
 import profileImg from "../../Assets/_General/Profile.svg";
 import cartImg from "../../Assets/_General/Cart.svg";
 import searchIconImg from "../../Assets/_General/Search.svg";
+import homePageImage from "../../Assets/Home/MainImage.png";
 
 import bgLineImg from "../../Assets/_General/BgLine.svg";
 
@@ -12,12 +13,20 @@ export const NAVBAR_DATA = {
   deleiverTo: "Deliver to",
   profie: "Profile",
   cart: "Cart",
-  login: "Login/Signup",
+  login: "Login / Signup",
   images: {
     LOGO_IMG: logo,
     LOCATION_PIN: locationPin,
     PROFILE_IMG: profileImg,
     CART_IMG: cartImg,
     SEARCH_ICON_IMG: searchIconImg,
+  },
+};
+
+export const HOME_DATA = {
+  title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam",
+  button: "See top picks ->",
+  images: {
+    MAIN_IMAGE: homePageImage,
   },
 };
