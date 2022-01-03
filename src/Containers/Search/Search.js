211c3c7ebe3bbb4b54107d7672d9c7c4e504a443
Raj <varasaniraj5@gmail.com>
2022-01-03 18:50:31 +0530

    Created search page

--- src/Containers/Search/Search.js
+++ src/Containers/Search/Search.js
@@ -1,27 +1,53 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
-import styles from "./App.module.css";
+import styles from "./Search.module.css";
+
+import SerachMainComp from "../../Components/SerachMainComp";
+import Preloader from "../../Components/Preloader";
+import Footer from "../../Components/Footer";
 
 function Search() {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
+
+  const [searchResults, setSearchResults] = useState(null);
 
   useEffect(() => {
+    setSearchResults(null);
     console.log(searchParams.get("search"));
+    // Get Search Results
+    setTimeout(() => {
+      getSearchResults();
+    }, 1000);
   }, [searchParams]);
 
-  useEffect(() => {
-    // setInterval(() => {
-    //   setSearchParams({
-    //     ...searchParams,
-    //     rnd: Math.random(),
-    //   });
-    // }, 5000);
-  }, []);
+  const getSearchResults = async () => {
+    setSearchResults(
+      Array(15)
+        .fill({})
+        .map((_, index) => ({
+          id: index,
+          title: `${searchParams.get("search")} Printed Men Hooded Neck Da..`,
+          seller: `Blive  enterprise ${index + 1}`,
+          price: Math.floor(Math.random() * 500) + 500,
+          rating:
+            Math.floor(Math.random() * 3) +
+            1 +
+            Math.floor(Math.random() * 10) / 10,
+          noOfRatings: Math.floor(Math.random() * 100) + 100,
+          image: `https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80`,
+        }))
+    );
+  };
 
   return (
-    <div>
-      <h1>{searchParams}</h1>
+    <div className={styles.Wrapper}>
+      {searchResults ? (
+        <SerachMainComp searchResults={searchResults} />
+      ) : (
+        <Preloader />
+      )}
+      <Footer />
     </div>
   );
 }
