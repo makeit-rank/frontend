0a16fed5fd55c06005e25bdc20f2f45ef96c869a
Raj <varasaniraj5@gmail.com>
2022-01-06 16:17:37 +0530

    Updated constants

--- src/Utils/Constants/ApiConstants.js
+++ src/Utils/Constants/ApiConstants.js
@@ -1,6 +1,12 @@
 export const PINCODE_TO_CITY_URL = "https://api.postalpincode.in/pincode/";
 
-export const BASE_URL = "https://makeit-test.azurewebsites.net/api/v1/";
-export const USER_URL = BASE_URL + "user/";
+export const BASE_URL =
+  "http://makeit.centralindia.cloudapp.azure.com:8080/api/v1/";
+export const PRIMARY_API =
+  "http://makeit.centralindia.cloudapp.azure.com:8080/api/v1/";
+export const STORAGE_API =
+  "http://makeit.centralindia.cloudapp.azure.com:8080/api/v1/";
+
+export const USER_URL = PRIMARY_API + "user/";
 
 export const GET_USER_DATA = USER_URL + "getuserdata";
