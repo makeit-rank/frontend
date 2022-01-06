a984995a55af543e071e509695c4f140040d5d28
Raj <varasaniraj5@gmail.com>
2022-01-06 21:05:48 +0530

    Created Cart page

--- src/Containers/Cart/Cart.js
+++ src/Containers/Cart/Cart.js
@@ -27,34 +27,34 @@ const Cart = () => {
     });
     console.log("get highlights data");
 
-    // const tempData = Array(5)
-    //   .fill({})
-    //   .map((_, index) => ({
-    //     id: index,
-    //     title: `Printed Men Hooded Neck Da..`,
-    //     seller: `Blive  enterprise ${index + 1}`,
-    //     price: Math.floor(Math.random() * 500) + 500,
-    //     rating:
-    //       Math.floor(Math.random() * 3) +
-    //       1 +
-    //       Math.floor(Math.random() * 10) / 10,
-    //     noOfRatings: Math.floor(Math.random() * 100) + 100,
-    //     image: `https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80`,
-    //   }));
+    const tempData = Array(4)
+      .fill({})
+      .map((_, index) => ({
+        id: index,
+        image: `https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80`,
+        title: `Printed Men Hooded Block Round Neck Black T-Shirt`,
+        seller: `Blive  enterprise ${index + 1}`,
+        size: "XL",
+        // size: Math.random() > 0.5 ? "S" : undefined,
+        price: Math.floor(Math.random() * 500) + 500,
+        attachedImages:
+          Math.random() > 0.5
+            ? [
+                `https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80`,
+                `https://images.unsplash.com/photo-1571945153237-4929e783af4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHsx8&auto=format&fit=crop&w=387&q=80`,
+                `https://images.unsplash.com/photo-1586790170083-2f9ceadc732d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80`,
+              ]
+            : undefined,
+      }));
 
-    const tempData = [];
-
-    setCartData({
-      recommended: tempData,
-      topPicks: tempData,
-    });
+    setCartData(tempData);
   };
 
   return (
     <div className={styles.Wrapper}>
       {cartData ? (
         cartData.length > 0 ? (
-          <CartMainSec cartData={cartData} />
+          <CartMainSec cartData={cartData} addresses={userData.address} />
         ) : (
           <EmptyCartComp />
         )
