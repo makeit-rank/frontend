d56b3a5ffb38c3486f2752babe4806e8b9e6d33e
Raj <varasaniraj5@gmail.com>
2022-01-11 12:10:08 +0530

    Created PrivateRoute

---
+++ src/Utils/Helpers/PrivateRoute.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({ inverted = false, children }) => {
+  const userData = useSelector((state) => state.userReducer.userData);
+
+  return (userData !== undefined) !== inverted ? (
+    children
+  ) : (
+    <Navigate to="/profile" />
+  );
+};
+
+export default PrivateRoute;
