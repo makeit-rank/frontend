5f46dc0b9495ba93de841156049eb08ddfdc6767
Raj <varasaniraj5@gmail.com>
2022-01-11 19:36:56 +0530

    Created add adress service

--- src/Services/user.service.js
+++ src/Services/user.service.js
@@ -1,9 +1,26 @@
 import axios from "axios";
-import { GET_USER_DATA } from "../Utils/Constants/ApiConstants";
+import {
+  GET_USER_DATA,
+  ADD_ADDESS_URL,
+  BECOME_A_SELLER_URL,
+} from "../Utils/Constants/ApiConstants";
 
 export const getUserData = async (accessToken) => {
   try {
     const { data } = await axios.get(GET_USER_DATA, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const addAddress = async (accessToken, address) => {
+  try {
+    const { data } = await axios.post(ADD_ADDESS_URL, address, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
