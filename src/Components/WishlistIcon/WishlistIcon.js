3a170b9a8e56eee5735b6c44a0e796d6dde9c2f5
Raj <varasaniraj5@gmail.com>
2022-01-13 01:19:21 +0530

    Bug fix >> Added check for userdata in WishlistIcon

--- src/Components/WishlistIcon/WishlistIcon.js
+++ src/Components/WishlistIcon/WishlistIcon.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import styles from "./WishlistIcon.module.css";
 
@@ -9,39 +10,48 @@ import notify from "./../../Utils/Helpers/notifyToast";
 import { addProductToWishlist } from "./../../Services/user.service";
 
 function WishlistIcon({ productId }) {
+  const navigate = useNavigate();
   const [selected, setSelected] = useState(false);
   const userData = useSelector((state) => state.userReducer.userData);
 
   useEffect(() => {
-    setSelected(userData.wishlist.includes(productId));
+    if (userData) {
+      setSelected(userData.wishlist.includes(productId));
+    } else {
+      setSelected(false);
+    }
   }, [productId]);
 
   const handleOnClick = async (e) => {
     e.preventDefault();
-    if (selected) {
-      try {
-        const result = await removeProductFromWishlist(
-          userData.accessToken,
-          productId
-        );
-        notify("Product removed from wishlist", "success");
-        setSelected(false);
-      } catch (err) {
-        console.log(err);
-        notify("Error removing product from wishlist", "error");
+    if (userData) {
+      if (selected) {
+        try {
+          const result = await removeProductFromWishlist(
+            userData.accessToken,
+            productId
+          );
+          notify("Product removed from wishlist", "success");
+          setSelected(false);
+        } catch (err) {
+          console.log(err);
+          notify("Error removing product from wishlist", "error");
+        }
+      } else {
+        try {
+          const result = await addProductToWishlist(
+            userData.accessToken,
+            productId
+          );
+          notify("Product added to wishlist", "success");
+          setSelected(true);
+        } catch (err) {
+          console.log(err);
+          notify("Error adding product to wishlist", "error");
+        }
       }
     } else {
-      try {
-        const result = await addProductToWishlist(
-          userData.accessToken,
-          productId
-        );
-        notify("Product added to wishlist", "success");
-        setSelected(true);
-      } catch (err) {
-        console.log(err);
-        notify("Error adding product to wishlist", "error");
-      }
+      navigate("/login");
     }
   };
 
