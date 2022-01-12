b81d6767213ed86d749dcd276019bf7c3cbd9308
Raj <varasaniraj5@gmail.com>
2022-01-13 01:37:26 +0530

    Removed garbage code

--- src/Components/ProfileMainSec/ProfileRightSec/OrdersSec/OrdersSec.js
+++ src/Components/ProfileMainSec/ProfileRightSec/OrdersSec/OrdersSec.js
@@ -26,33 +26,6 @@ function OrdersSec() {
       console.log(error);
       notify("Something went wrong", "error");
     }
-    // let tmpOrdersData = Array(7)
-    //   .fill({})
-    //   .map((_, index) => {
-    //     return {
-    //       id: Math.floor(Math.random() * 100000000),
-    //       timestamp: new Date().getTime(),
-    //       total: Math.floor(Math.random() * 1000),
-    //       productDetails: {
-    //         image: `https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80`,
-    //         title: `Printed Men Hooded Block Round Neck Black T-Shirt`,
-    //         seller: `Blive  Enterprise`,
-    //         size: "S",
-    //         // size: Math.random() > 0.5 ? "S" : undefined,
-    //         attachedFiles:
-    //           // Math.random() > 0.5
-    //           [
-    //             "https://images.unsplash.com/photo-1579783902614-a3fb3927b6a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=445&q=80",
-    //             "https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
-    //             "https://images.unsplash.com/photo-1509281373149-e957c6296406?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=428&q=80",
-    //             "https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
-    //             "https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    //           ],
-    //         // : undefined,
-    //       },
-    //     };
-    //   });
-    // setOrders(tmpOrdersData);
   };
 
   return (
